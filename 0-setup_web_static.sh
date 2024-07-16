aa5482d79e981a14f1b734184f3d215be446aa28
Kroser-draid <kingdomskrosergmail.com>
2024-07-16 07:39:23 +0100

    task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,18 +1,16 @@
-#!/usr/bin/env bash
-# a Bash script that sets up your web servers
-sudo apt-get update
-sudo apt-get install -y nginx
-sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/releases/test/
-echo "<!DOCTYPE html>
-<html>
-    <head>
-    </head>
-    <body>
-      Holberton School
-    </body>
-</html>" | sudo tee /data/web_static/releases/test/index.html
-sudo ln -fs /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data/
+#!/bin/bash
+
+sudo apt update -y
+sudo apt install nginx -y
+# Create required folders if it doesn't exists
+mkdir -p /data/web_static/shared/
+mkdir -p /data/web_static/releases/test/
+echo "<html><head></head><body> Holberton School </body></html>" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data/
+
+# Find the server block and insert the configuration
 sudo sed -i '39 i\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-enabled/default
+
+# Restart Nginx
 sudo service nginx restart
